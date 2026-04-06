b99fec4ab7d635c13f089960942bed8673956bc6
Bill-GD <geometrydashbill@gmail.com>
2026-04-06 14:46:32 +0700

    use knex schema helper functions
    - resource & field validator use knex built-in helper functions
    - kept wrapper for future ref

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -5,26 +5,28 @@ export function getDBClient() {
 }
 
 export async function checkField(tableName: string, field: string) {
-  const result = await db('information_schema.columns')
-    .where({
-      table_schema: 'public',
-      table_name: tableName,
-      column_name: field,
-    })
-    .count('column_name')
-    .first();
-  const count = +(result?.count ?? 0);
-  return count > 0;
+  // const result = await db('information_schema.columns')
+  //   .where({
+  //     table_schema: 'public',
+  //     table_name: tableName,
+  //     column_name: field,
+  //   })
+  //   .count('column_name')
+  //   .first();
+  // const count = +(result?.count ?? 0);
+  // return count > 0;
+  return db.schema.hasColumn(tableName, field);
 }
 
 export async function checkResource(tableName: string) {
-  const result = await db('information_schema.tables')
-    .where({
-      table_schema: 'public',
-      table_name: tableName,
-    })
-    .count('table_name')
-    .first();
-  const count = +(result?.count ?? 0);
-  return count > 0;
+  // const result = await db('information_schema.tables')
+  //   .where({
+  //     table_schema: 'public',
+  //     table_name: tableName,
+  //   })
+  //   .count('table_name')
+  //   .first();
+  // const count = +(result?.count ?? 0);
+  // return count > 0;
+  return db.schema.hasTable(tableName);
 }
